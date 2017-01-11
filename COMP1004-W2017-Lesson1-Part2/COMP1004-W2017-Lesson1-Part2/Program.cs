621df582ba9efb3f7ce5075d613d63bc59c0b332
YJY <Junyeong.Yu@MyGeorgian.ca>
2017-01-11 11:58:40 -0500

    commit program.cs

--- COMP1004-W2017-Lesson1-Part2/COMP1004-W2017-Lesson1-Part2/Program.cs
+++ COMP1004-W2017-Lesson1-Part2/COMP1004-W2017-Lesson1-Part2/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace COMP1004_W2017_Lesson1_Part2
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        class Person
         {
+            // public properties
+            public string name { get; set; }
+            public int age { get; set; }
+
+            // Constructor
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            public void saysHello()
+            {
+                Console.WriteLine("hello world in saysHello()");
+            }
+        }
+
+        static void Main()
+        {
+            Person tom = new Person("Tom", 30);
+            tom.saysHello();
+            Console.WriteLine("Hello World");
         }
     }
 }
